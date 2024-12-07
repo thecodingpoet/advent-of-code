c2107311a450a0de6a5f5e677219e07904521ca7
Tomi Duyile <thecodingpoet@gmail.com>
2024-12-07 23:29:56 +0100

    Solve part 2 of advent of code Day 7

--- 2024/day-7/main.rb
+++ 2024/day-7/main.rb
@@ -3,36 +3,42 @@ class BridgeRepair
 
   OPERATORS = {
     addition: '+',
-    multiplication: '*'
+    multiplication: '*',
+    concatenation: '||'
   }.freeze
 
   def initialize(file_name='input.txt')
     @calibration_equations = File.readlines(file_name)
   end
 
-  def total_calibration_result
-    @calibration_equations
-      .filter { |equation| valid_equation?(equation) }
-      .sum { |equation| equation.split(':').first.to_i }
+  def partial_calibration_total
+    calibration_total(OPERATORS.values - [OPERATORS[:concatenation]])
+  end
+
+  def complete_calibration_total
+    calibration_total(OPERATORS.values)
   end
 
   private
 
-  def valid_equation?(equation)
+  def calibration_total(allowed_operators)
+    @calibration_equations
+      .filter { |equation| evaluate_equation(equation, allowed_operators) }
+      .sum { |equation| equation.split(':').first.to_i }
+  end
+
+  def evaluate_equation(equation, allowed_operators)
     total, numbers = extract_values(equation)
 
-    outcomes = [numbers.shift]
     level = 0
+    outcomes = [numbers.shift]
 
-    while numbers.length > 0
-      next_num = numbers.shift 
-      
-      outcomes_at_current_level = outcomes.slice!(0, 2 ** level)
+    while numbers.any?
+      next_num = numbers.shift
+      outcomes_at_current_level = outcomes.slice!(0, allowed_operators.count ** level)
 
-      outcomes_at_current_level.each.with_index do |outcome, index|
-        OPERATORS.values.each do |operator|
-          outcomes << eval("#{outcome.to_s}#{operator}#{next_num}")
-        end
+      outcomes_at_current_level.each do |outcome|
+        outcomes.concat(generate_outcomes(outcome, next_num, allowed_operators))
       end
 
       level += 1
@@ -41,9 +47,23 @@ class BridgeRepair
     outcomes.include?(total.to_i)
   end
 
+  def generate_outcomes(outcome, next_num, allowed_operators)
+    allowed_operators.map do |operator|
+      case operator
+      when OPERATORS[:addition]
+        outcome + next_num
+      when OPERATORS[:multiplication]
+        outcome * next_num
+      when OPERATORS[:concatenation]
+        (outcome.to_s + next_num.to_s).to_i
+      end
+    end
+  end
+
   def extract_values(equation)
-    equation.split(':').then { |total, numbers| [total, numbers.split] }
+    equation.split(':').then { |total, numbers| [total, numbers.split.map(&:to_i)] }
   end
 end
 
-puts BridgeRepair.new.total_calibration_result
+puts BridgeRepair.new.partial_calibration_total
+puts BridgeRepair.new.complete_calibration_total
