6b5726a9cdd8ec3487ea5a053afeb5417be23bc7
Tomi Duyile <thecodingpoet@gmail.com>
2024-12-10 14:53:18 +0100

    Complete advent of code Day 10

--- 2024/day-10/main.rb
+++ 2024/day-10/main.rb
@@ -15,15 +15,30 @@ class TopographicMap
     sum
   end
 
+  def self.sum_of_scores_of_all_trailheads_part_2(input)
+    sum = 0
+
+    grid = input.split(/\n/).map { |line| line.chars.map(&:to_i) }
+    grid.each.with_index do |row, x|
+      row.each.with_index do |col, y|
+        next unless col == 0
+
+        sum += recursive_move_count(grid, x, y, true)
+      end
+    end
+    
+    sum
+  end
+
   private
 
-  def self.recursive_move_count(grid, x, y, result = Set.new)
+  def self.recursive_move_count(grid, x, y, allow_revisit = false, result = [])
     current_value = grid[x][y]
     next_value = current_value + 1
 
     if current_value == 9
       result << [x,y]
-      return result.length
+      return allow_revisit ? result.length : result.to_set.length
     end
 
     potential_moves = [
@@ -38,10 +53,10 @@ class TopographicMap
       value = grid[move_x][move_y]
       next unless value == next_value
 
-      recursive_move_count(grid, move_x, move_y, result)
+      recursive_move_count(grid, move_x, move_y, allow_revisit, result)
     end
 
-    return result.length
+    return allow_revisit ? result.length : result.to_set.length
   end
 
   def self.within_grid?(grid, x, y)
@@ -50,3 +65,4 @@ class TopographicMap
 end
 
 p TopographicMap.sum_of_scores_of_all_trailheads_part_1(File.read('input.txt'))
+p TopographicMap.sum_of_scores_of_all_trailheads_part_2(File.read('input.txt'))
